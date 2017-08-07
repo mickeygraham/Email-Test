1f8813e8bc9ad5e75bf18e022887540970cd4027
Mickey <mickey@work-bench.com>
2017-08-07 18:11:34 -0400

    latest versions

--- main.js
+++ main.js
@@ -1,34 +1,3 @@
-function MickeyTestFunction () {
-  document.getElementById("js-email-list").innerHTML = 5 + 6; 
-}
-
-var options = [
-        set0 = ['Option 1','Option 2'],
-        set1 = ['First Option','Second Option','Third Option']
-    ];
-
-function makeUL(array) {
-    // Create the list element:
-    var list = document.createElement('ul');
-
-    for(var i = 0; i < array.length; i++) {
-        // Create the list item:
-        var item = document.createElement('li');
-
-        // Set its contents:
-        item.appendChild(document.createTextNode(array[i]));
-
-        // Add it to the list:
-        list.appendChild(item);
-    }
-
-    // Finally, return the constructed list:
-    return list;
-}
-
-// Add the contents of options[0] to #foo:
-document.getElementById('foo').appendChild(makeUL(options[0]));
-
 
 /*
   TODO:
@@ -38,8 +7,7 @@ document.getElementById('foo').appendChild(makeUL(options[0]));
 */
 
 function render() {
-  // Your Code Here
-
+ 
   renderEmails();
 }
 
